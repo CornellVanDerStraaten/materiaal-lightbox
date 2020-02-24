d37450c415bc563a5e5183fba1c7ad4463e12cf9
Cornell van der Straaten <30183@ma-web.nl>
2020-02-24 20:12:24 +0100

    Added javascript and css

--- js/lightbox.js
+++ js/lightbox.js
@@ -6,4 +6,37 @@ const allBig = [];
 
 for(let i = 0; i < toDelete.length; i++) {
     allBig.push(toDelete[i]);
+    // Remove element from DOM //
+    toDelete[i].remove();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+}
+
+const makeCloseButton = document.createElement('i');
+makeCloseButton.className = 'fas fa-times-circle sk';
+makeCloseButton.addEventListener('click', closePopup);
+
+function makePopup(num) {
+    let popup = document.createElement('div');
+    popup.id = 'popup';
+    popup.addEventListener('click', closePopup);
+    let popupContent = document.createElement('div');
+    popupContent.className = 'popupContent';
+    popupContent.innerHTML = allBig[num].innerHTML;
+    popupContent.addEventListener('click', function(e){
+        e.stopPropagation();
+    })
+    popupContent.prepend(makeCloseButton);
+    popup.append(popupContent);
+    document.body.append(popup);
+}
+
+function closePopup() {
+    document.getElementById('popup').remove();
+}
+
+
+// onClick events on .small Divs
+for(let i = 0; i < smallItems.length; i++) {
+    smallItems[i].addEventListener('click', function(){
+        makePopup(i);
+    });
 }
